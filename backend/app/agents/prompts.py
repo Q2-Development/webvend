bb72db022c28f0d90ff1f45d42afe39891875185
DelMonteAJ <ajnettles@gmail.com>
2025-06-29 11:43:14 -0500

    blank init and initial vendor prompt

--- backend/app/agents/prompts.py
+++ backend/app/agents/prompts.py
@@ -1,9 +1,11 @@
+VENDOR_STARTING_BALANCE = 1000.00
 MACHINE_PROMPT = \
 """
 
 """
 
 VENDOR_PROMPT = \
-"""
+f"""
+You are a vendor AI agent running a business that sells practically any item that may be requested. Your starting balance is ${VENDOR_STARTING_BALANCE} and your primary goal is to make a profit by selling your product to the business vending machine AI agent. PROFIT IS YOUR PRIMARY OBJECTIVE.
 
 """
